f2572928fe39987bac39fefc54a6c7d77149891b
DeyvesDion <deyvesdion@gmail.com>
2021-07-19 10:39:52 +0200

    structure html

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,24 @@
 <template>
     <div id="app">
-        <div id="nav"><router-link to="/">Home</router-link> |</div>
-        <router-view />
+        <!-- <div id="nav"><router-link to="/">Home</router-link> |</div>
+        <router-view /> -->
+        <Accueil />
     </div>
 </template>
-
+<script>
+import Accueil from "@/components/Accueil.vue";
+export default {
+    components: {
+        Accueil,
+    },
+};
+</script>
 <style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
 * {
     margin: 0;
     padding: 0;
     list-style: none;
     background-size: cover;
     box-sizing: border-box;
-}
-
-#nav {
-    padding: 30px;
-    background-color: red;
-}
-
-#nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-    color: #42b983;
 }
 </style>
--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -1,5 +1,42 @@
 <template>
-    <div class="hello"></div>
+    <div class="accueil">
+        <div class="accueil__banner">
+            <div class="icon__wrapper">
+                <span class="icon__left circle"
+                    ><i class="fas fa-arrow-left"></i
+                ></span>
+                <span class="icon__right circle"
+                    ><i class="fas fa-arrow-right"></i
+                ></span>
+                <span class="icon__refresh circle"
+                    ><i class="fas fa-redo-alt"></i
+                ></span>
+            </div>
+            <div class="search__bar">
+                <div class="search__form">
+                    <div class="item_left">
+                        <span class="icon__left circle"
+                            ><i class="fas fa-lock"></i
+                        ></span>
+                        <span class="search__title"
+                            ><h3>localhost : 8080</h3></span
+                        >
+                    </div>
+                    <div class="item_right circle">
+                        <span class="icon__right"
+                            ><i class="far fa-star"></i
+                        ></span>
+                    </div>
+                </div>
+            </div>
+            <div class="user">
+                <div class="user__img circle"></div>
+                <span class="user__icon circle"
+                    ><i class="fas fa-ellipsis-v"></i
+                ></span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -10,15 +47,72 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-ul {
-    list-style-type: none;
-    padding: 0;
+.accueil__banner {
+    height: 70px;
+    /* background-color: red; */
 }
-li {
-    display: inline-block;
-    margin: 0 10px;
+.accueil__banner,
+.icon__wrapper,
+.user,
+.search__form,
+.item_left {
+    display: flex;
 }
-a {
-    color: #42b983;
+.icon__wrapper {
+    width: 8.5%;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+}
+.search__bar {
+    width: 65.6%;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+}
+.search__form {
+    width: 100%;
+    border-radius: 25px;
+    background-color: rgba(128, 128, 128, 0.11);
+    align-items: center;
+    justify-content: space-between;
+    /* display: flex; */
+}
+.item_left {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.user {
+    width: 25.9%;
+    align-items: center;
+    justify-content: space-between;
+    display: flex;
+}
+.circle {
+    height: 28px;
+    width: 28px;
+    cursor: pointer;
+    background-color: transparent;
+    border-radius: 50%;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    /* display: inline-block; */
+}
+.circle:hover {
+    background-color: rgba(128, 128, 128, 0.11);
+}
+.fas,
+.far {
+    font-size: 17px;
+    font-weight: 900;
+}
+.search__bar {
+    justify-content: space-between;
+}
+.item_left {
+}
+.user {
 }
 </style>
